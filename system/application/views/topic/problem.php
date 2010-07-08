ec4d5e619bb65c5e9fd5071e08cb58608441f79b
unknown <Trav@.(none)>
2010-07-08 13:47:06 -0700

    -updated views/topic/search.php -updated views/topic/problem.php -edited footer syner icon.

--- system/application/views/topic/problem.php
+++ system/application/views/topic/problem.php
@@ -1,6 +1,36 @@
 <!-- div body tag opens in " ".php and closes in footer.php-->
-<form src="problemajax" name="mceeditor">
-	<div id="tiny_mce_div" class="tiny_mce_div">
-	</div>
-</form>
-<a href="javascript:toggle_editor();">Disable/Enable view</a>
+<div id="body">
+	<div id="bigBox">
+			<div class="topicnav" id="problem">
+				<ul id="topicnav"> 
+					<li class="problem"><a href=""><img src="<?=SY_SITEPATH?>styles/icons/problem_ico.png" border="0" /> Problem</a></li> 
+					<li class="discussion"><a href=""><img src="<?=SY_SITEPATH?>styles/icons/discussion_ico.png" border="0" /> Discussion</a></li> 
+					<li class="solutions"><a href=""><img src="<?=SY_SITEPATH?>styles/icons/solutions2_ico.png" border="0" /> Solutions</a></li> 
+					<li class="action"><a href=""><img src="<?=SY_SITEPATH?>styles/icons/action_ico.png" border="0" /> Action</a></li> 
+				</ul> 
+			</div>
+			
+			<div class="controlnav">
+				<ul id="controlnav"> 
+					<li class="edit"><a href="javascript:toggle_editor();"><img src="<?=SY_SITEPATH?>styles/icons/edit_ico.png" border="0" /> Edit</a></li> 
+				</ul> 
+			</div>
+			
+			<div id="content" class="topic problem">
+				<div id="titleDisplay">
+				Topic Title Field
+				</div>
+				<p>
+				<!-- Tiny MCE form display -->
+					<form src="problemajax" name="mceeditor">
+					<div id="tiny_mce_div" class="tiny_mce_div">
+					</div>
+					</form>
+				</p>
+				<p>
+				Basically when a user would approach a problem for the first time, they would see the Title, Tags, and Description in plain text.  When they hit the "Edit" button then the page loads TinyMCE for the problem description, and then two separate fields for the title, and tags.  When the user selects a button "Save" then then their changes are placed, an action is created that defines the changes and places it within the history feature (action page) and they're returned to the page in non-editor mode (plain text).  It would probably be helpful to have a "preview" option before the user submits.  It may even be helpful if we force the user to preview before submitting.  I see TinyMCE has a built in preview option.
+				</p>
+				
+			</div>
+			
+		</div>
--- system/application/views/topic/search.php
+++ system/application/views/topic/search.php
@@ -1,3 +1,41 @@
-<p>
-This is the view used for searching. It uses the layout.php. It can be changed however. <a href="problem/topic_id/0">Problem submission page for topic 0</a>
-</p>
+<div id="body">
+	
+		<div id="leftBox">
+			<div class="groupMod">
+				<ul class="groupBox">
+				<li id="title"><img src="<?=SY_SITEPATH?>styles/icons/groups3_ico.png" border="0" /> Groups</li>
+				<li><a href="" class="normal"><img src="<?=SY_SITEPATH?>styles/icons/global_ico.png" border="0" /> Global</a></li>
+				<li><a href="" class="normal"><img src="<?=SY_SITEPATH?>styles/icons/country_ico.png" border="0" /> Country</a></li>
+				<li><a href="" class=""><img src="<?=SY_SITEPATH?>styles/icons/personal_ico.png" border="0" /> Personal</a></li>
+				</ul>
+			</div>
+			
+		</div>
+		<div id="rightBox">
+			<div id="content">
+				<br />
+				<form>
+					<input type="text" name="search" value="" size="40" />
+					<input type="submit" value="Search" />
+				</form>
+				<div id="spacer"></div>
+				
+				<p>
+					This is the view used for searching. It uses the layout.php. But can be changed via search.php view.
+				</p>
+				<p>
+				<a href="problem/topic_id/0">Click here to view the problem submission page for topic 0</a>
+				</p>
+				<p>
+					We could include the submit option at the bottom of this search page.  It could also appear at the bottom of the search result listing.  As long as we encourage some type of "seach before submit" mentality.
+				</p>
+				
+				
+				
+			</div>
+			
+		</div>
+
+
+
+
