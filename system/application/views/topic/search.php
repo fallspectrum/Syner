2b803cdadc6988f467409b9e8c222042e94b112f
Xerond <xerond@gmail.com>
2010-07-09 17:25:38 -0700

    -updated topic views with additional styling/content

--- system/application/views/topic/search.php
+++ system/application/views/topic/search.php
@@ -4,7 +4,7 @@
 			<div class="groupMod">
 				<ul class="groupBox">
 				<li id="title"><img src="<?=SY_SITEPATH?>styles/icons/groups3_ico.png" border="0" /> Groups</li>
-				<li><a href="" class="normal"><img src="<?=SY_SITEPATH?>styles/icons/global_ico.png" border="0" /> Global</a></li>
+				<li><a href="" class="normal select"><img src="<?=SY_SITEPATH?>styles/icons/global_ico.png" border="0" /> Global</a></li>
 				<li><a href="" class="normal"><img src="<?=SY_SITEPATH?>styles/icons/country_ico.png" border="0" /> Country</a></li>
 				<li><a href="" class=""><img src="<?=SY_SITEPATH?>styles/icons/personal_ico.png" border="0" /> Personal</a></li>
 				</ul>
@@ -19,18 +19,26 @@
 					<input type="submit" value="Search" />
 				</form>
 				<div id="spacer"></div>
-				
 				<p>
-					This is the view used for searching. It uses the layout.php. But can be changed via search.php view.
+				<span>
+				To start your search, your first step is to select the group that you would like to search.  Global is selected by default, but if you would like to expand your search to more groups, click the checkbox "Include Sub-Groups."  
+				</span>
+				</p>
+				<p>
+					This is the search.php view.  
 				</p>
 				<p>
 				<a href="problem/topic_id/0">Click here to view topic 0</a><br />
-				<a href="submit/">Click here to view problem submission</a>
-				</p>
-				<p>
-					We could include the submit option at the bottom of this search page.  It could also appear at the bottom of the search result listing.  As long as we encourage some type of "seach before submit" mentality.
 				</p>
 				
+				
+				<p>
+				Before a user places a search, this part of this content area displays instructions for what to do.  After a user places a search, the results then replace the instructions and display a list.  This area here would be the resuts listing. Inside also contains page control options such as (next page, previous page, # of items per page, etc).
+				</p>
+				<div id="spacer"></div>
+				<p>
+				Can't find what you're looking for?  <a href="submit/"><b>Submit A Problem</b></a>
+				</p>
 				
 				
 			</div>
--- system/application/views/topic/submit.php
+++ system/application/views/topic/submit.php
@@ -14,9 +14,17 @@
 		<div id="rightBox">
 			<div id="content">
 				<b>Problem Submission Form:</b><br />
-				<p>Includes Title, Description, Tags, and Group.  We could employ a tag suggestion feature that scans the contents of the description that the user has typed, and based on that input would suggest tags for them to use: <a href="http://swift.ushahidi.com/extend/silcc/">http://swift.ushahidi.com/extend/silcc/</a></p>
-				<p>After a user submits this form a topic is created based on the input.  The user can continue to edit their topic from there.</p>
-				
+				<p><span>Step 1 - Select the group that this problem belongs to.  *hint icon, question mark*<br />
+				<------------</span></p>
+				<p>When a user clicks the hint icon, they're given details about what each group is for, Global is for a problem that affects all people, country for country, personal are for personal problems.</p>
+				<p><span>Step 2 - Title your problem.  *hint icon*</span></p>
+				<p>When the user clicks the hint icon, they are told to summarize the problem in a short sentence.</p>
+				<p><span>Step 3 - Describe your problem.  *hint icon*</span></p>
+				<p>This is where the Tiny_MCE editor would be displayed.  If a user clicks the hint icon, they're given details about the tiny-MCE abilities, such as linking to outside/inside content, images, videos, etc.</p>
+				<p><span>Step 4 - Tag your problem.  *hint icon*</span></p>
+				<p>This field should have some sort of suggestion feature above or near it.  An example of a tag suggestion feature is:  <a href="http://swift.ushahidi.com/extend/silcc/">SiLCC</a>.  SiLCC takes the description content, and analyzes it to make quick suggestions for tags for the user.  When the user clicks the hint icon, they're told that tags are used to categorize problems, and are also used to help people find problem they're interested in.  
+				<p><a href=""><b>SUBMIT</b></a></p>
+				<p><i>Notes: maybe during the submission process, rather than having "hint icons" we could allocate the right part of the form to display input instructions while the user's cursor is within that field.  For example, when the user first selects within the title field, a display area tells the user some details about making a good title. When they move from title to description, the title helpful fades out, and the description helper fades in.  </i></p>
 				
 			</div>
 			
