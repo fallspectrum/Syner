8eb1ef76ce342587c0cb1bba936ef17ba704ca26
Justin <thekeyboardbum@gmail.com>
2010-07-19 01:01:05 -0500

    Topic model updated
    models/topics.php changes:
    -Added flag constats at the top. These are used with get_topic_data.
    -Function get_topic_data ($topic_id,$flags) added. This will return the
    topic data used for viewing/editing. $flags variable takes in constants
    defined in models/topics.php. These constats should be ORed together in
    order to select what columns you want. Check comments in file for more
    details.

--- system/application/models/topics.php
+++ system/application/models/topics.php
@@ -6,6 +6,12 @@
  
 class Topics extends Model 
 {
+	/**
+	* Constants used for flags in topic_get_data
+	*/
+	const TOPIC_TITLE = 1;
+	const TOPIC_CONTENT =  2;
+
 	function Topics()
 	{
 		parent::Model();
@@ -80,6 +86,45 @@ class Topics extends Model
 		}
 		
 		return $query;
-	}	
+	}
+
+	/**
+	* This function retrives data related to the topic id. 
+	* @throws exception when no columns are selected
+	* @return a associtave array for selected column data.
+	*/
+	function get_topic_data($topic_id, $flags) {
+
+		$need_join = FALSE;
+		
+		
+		$this->db->from("topics");
+
+		//check to see if we need to preform a join. This should only occur when
+		//a table besides 'topic' is  needed in the queue.
+		if (Topics::TOPIC_CONTENT) {
+			$need_join;
+		}
+		
+
+		if($flags & Topics::TOPIC_TITLE) {
+			$this->db->select('topics.title');
+		}
+		if($flags & Topics::TOPIC_CONTENT) {
+			$this->db->select('topic_contents.content');
+			if($need_join) {
+				$this->db->join('topic_contents', 'topics.id = topic_contents.topic_id');
+			}
+		}
+
+		$this->db->where("topics.id", $topic_id);
+		$query = $this->db->get();
+		//nothing in the query
+		if($query->num_rows() <= 0) {
+			throw new Exception("Failed to get topic data.");
+		}
+		$result = $query->result_array();
+		return $result[0];
+	}
 	
 }
