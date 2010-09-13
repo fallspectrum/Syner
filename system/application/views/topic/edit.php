02340215d0391089cf1dc25aca0dd474fac6c99b
Justin <thekeyboardbum@gmail.com>
2010-09-12 22:55:06 -0500

    Fixed broken links on topic pages
    action.php, edit.php, problem.php changes:
    -Problem link was pointing to the incorrect place.

--- system/application/views/topic/edit.php
+++ system/application/views/topic/edit.php
@@ -3,7 +3,7 @@
 	<div id="bigBox">
 			<div class="topicnav" id="problem">
 				<ul id="topicnav"> 
-					<li class="problem"><a href="../../view/topic_id/<?=$topic_id?>"><img src="<?=SY_SITEPATH?>styles/icons/problem_ico.png" border="0" /> Problem</a></li> 
+					<li class="problem"><a href="<?=SY_SITEPATH?>index.php/topic/view/topic_id/<?=$topic_id?>"><img src="<?=SY_SITEPATH?>styles/icons/problem_ico.png" border="0" /> Problem</a></li> 
 					<li class="discussion"><a href=""><img src="<?=SY_SITEPATH?>styles/icons/discussion_ico.png" border="0" /> Discussion</a></li> 
 					<li class="solutions"><a href=""><img src="<?=SY_SITEPATH?>styles/icons/solutions2_ico.png" border="0" /> Solutions</a></li> 
 					<li class="action"><a href="<?=SY_SITEPATH?>index.php/topic/action/topic_id/<?=$topic_id?>"><img src="<?=SY_SITEPATH?>styles/icons/action_ico.png" border="0" /> Action</a></li>
